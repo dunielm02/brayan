51f793c0ed36e87fa9dc600bc337ee8f8687a5d7
Jose Carlos <joseserize0222@gmail.com>
2023-01-12 18:48:18 -0500

    cliente2

--- Back/Cliente.java
+++ Back/Cliente.java
@@ -3,13 +3,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Cliente {
-    private String id;
+    private String id="";
+    private String tarjeta="";
     private List<Pedido> lista_pedidos = new ArrayList<>();
-    public Cliente(String Id){
+    public Cliente(String Id, String Tarjeta){
         this.id = Id;
+        this.tarjeta = Tarjeta;
+    }
+    public Cliente(){
+        
     }
     public String getId() {
         return id;
+    }
+    public String getTarjeta(){
+        return this.tarjeta;
+    }
+    public void setTarjeta(String Tarjeta){
+        this.tarjeta = Tarjeta;
     }
     public void setId(String id) {
         this.id = id;
